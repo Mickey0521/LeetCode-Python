3d58fc1f1b7e14731eafd22fbfef0c60a1d76c36
Chien-Chi Kao <smallfriend521@gmail.com>
2020-10-31 19:47:30 +0800

    Update and rename 8_String_to_Integer_atoi_Medium.py to string_to_integer.py

--- string_to_integer.py
+++ string_to_integer.py
@@ -1,30 +1,3 @@
-8. String to Integer (atoi)
-Medium
-
-1517
-
-8979
-
-Add to List
-
-Share
-Implement atoi which converts a string to an integer.
-
-The function first discards as many whitespace characters as necessary until the first non-whitespace character is found. Then, starting from this character, takes an optional initial plus or minus sign followed by as many numerical digits as possible, and interprets them as a numerical value.
-
-The string can contain additional characters after those that form the integral number, which are ignored and have no effect on the behavior of this function.
-
-If the first sequence of non-whitespace characters in str is not a valid integral number, or if no such sequence exists because either str is empty or it contains only whitespace characters, no conversion is performed.
-
-If no valid conversion could be performed, a zero value is returned.
-
-Note:
-
-Only the space character ' ' is considered as whitespace character.
-Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. If the numerical value is out of the range of representable values, INT_MAX (231 − 1) or INT_MIN (−231) is returned.
-
-##########################################################################
-
 class Solution:
     def myAtoi(self, str: str) -> int:
         
@@ -84,13 +57,3 @@ class Solution:
             i += 1
 
         return sign * my_digit
-
-##########################################################################
-
-Success
-Details 
-Runtime: 44 ms, faster than 14.53% of Python3 online submissions for String to Integer (atoi).
-Memory Usage: 14 MB, less than 5.23% of Python3 online submissions for String to Integer (atoi).
-
-##########################################################################
-
